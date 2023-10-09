becb11307e8844affdb53bda8f0bbf290cd7791a
Zarzajpg <147415904+Zarzajpg@users.noreply.github.com>
2023-10-09 11:31:48 -0300

    Update proyectoG7.py

--- proyectoG7.py
+++ proyectoG7.py
@@ -1,2 +1,15 @@
 #Proyecto G 7 
-#Alumnos:
+#Alumnos:Zarza, Juan Pablo
+
+# Lista de palabras predefinidas
+palabras = ["informatorio", "septima", "programacion", "boquita", "libertadores", "chiquito"]
+
+# Función para seleccionar una palabra aleatoria
+def seleccionar_palabra():
+    return random.choice(palabras)
+
+# Función para inicializar el juego
+def iniciar_juego():
+    palabra_secreta = seleccionar_palabra()
+    letras_adivinadas = []
+    intentos_restantes = 6
